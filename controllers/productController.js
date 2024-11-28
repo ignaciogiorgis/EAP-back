c11810935805440574056f51ebbe8cea407b2c5f
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2024-11-27 21:34:08 -0300

    38-create-controler-show-all-products

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,4 +57,99 @@ const createProduct = async (req, res) => {
   });
 };
 
-export { createProduct };
+const showProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      where: {
+        isDeleted: false,
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Error obtaining products:", error);
+    res
+      .status(500)
+      .json({ error: "There was a problem obtaining the products" });
+  }
+};
+
+const editProduct = async (req, res) => {
+  await check("name")
+    .notEmpty()
+    .withMessage("The name cannot be empty")
+    .run(req);
+  await check("quantity")
+    .notEmpty()
+    .isFloat()
+    .withMessage("The quantity cannot be empty ")
+    .run(req);
+  await check("cost")
+    .notEmpty()
+    .isFloat()
+    .withMessage("The cost cannot be empty")
+    .run(req);
+  await check("profit")
+    .notEmpty()
+    .isFloat()
+    .withMessage("The profit cannot be empty")
+    .run(req);
+
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.status(400).json({ error: result.array() });
+  }
+
+  const { id } = req.params;
+  const { name, quantity, cost, profit } = req.body;
+
+  try {
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not Found" });
+    }
+
+    await product.update({
+      name: name ?? product.name,
+      quantity: quantity ?? product.quantity,
+      cost: cost ?? product.cost,
+      profit: profit ?? product.profit,
+    });
+
+    return res.status(200).json({
+      message: "Product updated correctly",
+      product,
+    });
+  } catch (error) {
+    console.error("Error updating Product:", error);
+    return res
+      .status(500)
+      .json({ error: "There was a problem updating the Product" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Busca el gasto por su ID
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Actualiza el campo `isDeleted` a true
+    await product.update({ isDeleted: true });
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export { createProduct, showProducts, editProduct, deleteProduct };
