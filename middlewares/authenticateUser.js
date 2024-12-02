11691232b37c38c0c7a8eca79b2e0e22e79ef248
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2024-12-02 17:12:30 -0300

    44-fix-idUser-in-expenses

---
+++ middlewares/authenticateUser.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.id }; // Almacena el ID del usuario en la solicitud
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+export default authenticateUser;
