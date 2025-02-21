4c08be74918d58367af0dea4eead112e9036f1fc
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2025-02-21 12:53:26 -0300

    fix-app-deploy-01

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const db = new Sequelize(
   process.env.BD_PASSWORD ?? "",
   {
     host: process.env.BD_HOST,
-    port: "3306", 
+    port: process.env.BD_PORT ?? "3306", // Usa la variable BD_PORT si existe
     dialect: "mysql",
     define: {
       timestamps: true,
@@ -22,5 +22,17 @@ const db = new Sequelize(
     },
   }
 );
+
+async function testConnection() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.message);
+    console.error("Detailed error:", error);
+  }
+}
+
+testConnection();
 
 module.exports = db;
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
