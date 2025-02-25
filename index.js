a82bf30de802a1d85ab81126c29be41c2492a290
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2025-02-25 16:25:22 -0300

    fix-deploy5

--- index.js
+++ index.js
@@ -15,15 +15,21 @@ app.use(
     extended: true,
   })
 );
-
+console.log("FRONTEND_URL configurado:", process.env.FRONTEND_URL);
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://eap-front.vercel.app/",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use((req, res, next) => {
+  console.log(
+    `Petición recibida: ${req.method} ${req.url} desde ${req.headers.origin}`
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.static("public"));
