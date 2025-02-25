ffee7cf8024b4171b808298c6a19368c91af6ad1
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2025-02-24 22:59:51 -0300

    fix-deploy3

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.BD_USER,
-    password: process.env.BD_PASSWORD ?? "",
+    password: process.env.BD_PASSWORD || "",
     database: process.env.BD_NOMBRE,
     host: process.env.BD_HOST,
     port: process.env.BD_PORT || "3306",
